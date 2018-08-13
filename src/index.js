040ade54328a9a3204981331cc6d175ddcc590b7
pethaf <peter@ctrl-c.liu.se>
2018-08-13 16:00:06 +0200

    Fix title in HTML page, add https to quote API

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const QUOTEURL = "http://api.icndb.com/jokes/random?escape=javascript";
+const QUOTEURL = "https://api.icndb.com/jokes/random?escape=javascript";
 const TWEETURL = "https://twitter.com/intent/tweet";
 ReactDOM.render(<App quoteURL = {QUOTEURL} tweetURL = {TWEETURL} />, document.getElementById('root'));
 registerServiceWorker();
